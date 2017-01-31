f3ae97b3797d4f9dc01dd29815bc78186f781f63
Brady O'Leary <nesdood007@gmail.com>
2017-01-30 23:43:50 -0500

    Wrote code for prob4.C

--- Worksheets/1/prob4.C
+++ Worksheets/1/prob4.C
@@ -13,6 +13,27 @@ int main() {
     //Write code here.
     //Don't forget to print out the array from pointer c
     
+    int a_size = 5;
+    int b_size = 5;
+
+    c = new int[a_size + b_size];
+
+    for (int i = 0; i < a_size + b_size ; i++) {
+        if (i < a_size) {
+            c[i] = a[i];
+        } else {
+            c[i] = b[i - b_size];
+        }
+    }
+
+    for (int i = 0; i < a_size + b_size; i++) {
+        cout << c[i];
+    }
+
+    cout << endl;
+
+    delete [] c;
+
     return 0;
 }
 
