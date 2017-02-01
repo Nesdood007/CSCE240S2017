d24b2f5a90177eda5b6fc64f9445716a885ba979
Brady O'Leary <nesdood007@gmail.com>
2017-02-01 16:44:01 -0500

    Auto-Compile Script does not work yet

--- Worksheets/ExamReview1/runall.sh
+++ Worksheets/ExamReview1/runall.sh
@@ -2,7 +2,10 @@
 
 #This is a script that will compile, run and store the output of each problem in file: "output"
 
-#DIRS= $(find -maxdepth 2);
 EXEC= exec;
 #(cd (cat $DIRS) && make && ./exec >> output);
-(cd `find -maxdepth 2 -mindepth 2 -not -name README.md` && make && ./$EXEC >> output);
+#(cd `find -maxdepth 2 -mindepth 2 -not -name README.md` && (make && ./$EXEC >> output));
+
+#(cd `find -maxdepth 2 -mindepth 2 -not -name README.md` && echo $PWD);
+
+find -maxdepth 2 -mindepth 2 -not -name README.md -exec echo $(echo {} &&  cd {} && make && ./$EXEC >> output) \;
