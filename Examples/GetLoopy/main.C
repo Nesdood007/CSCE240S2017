aa81dcdd8c2dfe4cdb97abd9e1d2baea57d69724
Brady O'Leary <nesdood007@gmail.com>
2017-02-14 12:47:03 -0500

    Moved HelloWorld and GetLoopy Examples

---
+++ Examples/GetLoopy/main.C
@@ -0,0 +1,24 @@
+/*  Loop and Conditional Examples in C
+        For Loops
+        While Loops
+        IF Statements
+        DoWhile Loops
+        Good Variable Names
+        Postinc/Preinc
+        
+        
+        
+        CSCE240 Spring 2017
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    
+    cout << "Demonstration of For Loops" << endl;
+    
+    
+    
+}
---
+++ Examples/HelloWorld/main.C
@@ -0,0 +1,18 @@
+/** Hello World Example:
+*
+*   This is a program that upon execution prints out the words "Hello World"
+*   into the Standard Output of the terminal.
+*
+*   CSCE240S2017
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+        
+    cout << "Hello World" << endl;
+        
+    return 0;
+}
